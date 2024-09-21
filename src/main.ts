462612995f628912ca3952496d4a189c0ce39bd2
kiethohoai <kietho.060193@gmail.com>
2024-09-21 19:46:44 +0700

    Chapter 3: Arrays & Objects

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,53 @@
-const sum = (a: number, b: number) => {
-  return a + b;
-};
-console.log("🚀  sum: ", sum(1, 2));
+// let bands: string[] = [];
+// bands.push("Metallica", "Nirvana", "Pearl Jam");
+// console.log(bands);
+
+// Tuple
+// let myTuple: [string, number, boolean] = ["Metallica", 1980, true];
+// console.log("🚀  myTuple: ", myTuple);
+// let mixed = ["John", 30, true];
+// console.log("🚀  mixed: ", mixed);
+
+// Object
+// let myObj: object = {
+//   name: "John",
+//   age: 30,
+// };
+
+// myObj = [];
+// console.log("🚀  myObj: ", typeof myObj);
+// let temp: string[] = ["1", "2"];
+// myObj = temp;
+// console.log("🚀  myObj: ", myObj);
+
+// let myObj = {
+//   p1: "John",
+//   p2: true,
+// };
+// myObj.p1 = 3934875;
+// myObj.p2 = "false";
+
+// type guitar = {
+//   name: string;
+//   year: number;
+//   active: boolean;
+//   albums: (string | number)[];
+// };
+
+// let myGuitar: guitar = {
+//   name: "John",
+//   year: 1980,
+//   active: true,
+//   albums: ["My 1 life", 123, 38472, "2342fkjew"],
+// };
+// console.log("🚀  myGuitar: ", myGuitar);
+
+// Enum
+enum Direction {
+  Up = 1,
+  Down,
+  Left,
+  Right,
+}
+
+console.log("🚀  Direction: ", Direction);
