e8ae6adaa178838c80475b51a17b2d119318fb16
kiethohoai <kietho.060193@gmail.com>
2024-09-21 09:34:09 +0700

    008 Object Types

--- app.ts
+++ app.ts
@@ -1,15 +1,11 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-  const result = n1 + n2;
-  if (showResult) {
-    console.log(phrase + result);
-  } else {
-    return result;
-  }
-}
+// type typePerson = {
+//   name: string;
+//   age: number;
+// };
 
-const n1 = 5;
-const n2 = 2.8;
-const showResult = true;
-const phrase = "Result is: ";
-
-const finalResult = add(n1, n2, showResult, phrase);
+const person = {
+  name: "John",
+  age: 30,
+};
+console.log("🚀🚀🚀  person.age: ", person.age);
+console.log("🚀🚀🚀  person.name: ", person.name);
