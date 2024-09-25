f74f32469e5c8257beb9cfb9a039d0a41b5553a9
kiethohoai <kietho.060193@gmail.com>
2024-09-25 15:21:38 +0700

    Chapter 10: Vite.js + TypeScript

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-interface Todo {
-  title: string;
-  description: string;
-  completed: boolean;
-}
-
-type TodoPreview = Omit<Todo, "completed">;
-const todo: TodoPreview = { title: "Hello", description: "World" };
